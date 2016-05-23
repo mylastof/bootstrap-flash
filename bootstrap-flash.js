996944badbcff472d307f3bf424dd7d7f6442f32
mylastof <mylastof@gmail.com>
2016-05-23 13:00:46 +0700

    fixed show hide

--- bootstrap-flash.js
+++ bootstrap-flash.js
@@ -1,6 +1,6 @@
 /*
     Bootstrap Flash
-    Version: 1.0
+    Version: 3.0
     A small lib to display notifications by @vsergeyev
     https://github.com/vsergeyev/bootstrap-flash
 
@@ -9,23 +9,22 @@
 
 (function(){
     $(document).ready(function () {
-        $("body").prepend('<div class="bootstrap-flash alert alert-success alert-danger hide" style="position:absolute;top:1px;left:25%;width:50%;float:left;z-index:1500;"><a class="close" href="#" onclick="$(this).parent().hide();return false;">&times;</a><p style="margin:0!important"></p></div>');
+        $("body").prepend('<div class="bootstrap-flash alert alert-success alert-danger hide" role="alert" style="position:absolute;top:1px;left:25%;width:50%;float:left;z-index:1500;"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><p></p></div>');
     });
 
     // Hide alert in 10 seconds
     setInterval(function() {
-        $(".bootstrap-flash").fadeOut("slow");
+        $(".bootstrap-flash").removeClass('show').fadeOut("slow");
     }, 10000);
 
     this.flash = function(msg, err) {
         var box = $(".bootstrap-flash");
         if (err)
-            box.addClass("alert-danger show")
+            box.addClass("alert-danger")
         else
-            box.removeClass("alert-danger"),
-            box.addClass("show");
+            box.removeClass("alert-danger");
 
         box.find("p").html(msg);
-        box.show();
+        box.addClass("show");
     }
 }).call(this);
